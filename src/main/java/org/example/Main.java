24848d08339bca471b4de212fa20d7cfb12e117a
ramru <ramrush-r@mail.ru>
2023-07-09 17:39:59 +0300

    init commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) throws InterruptedException {
-        System.out.println("Works");
+        System.out.println("Works for me");
     }
 }
